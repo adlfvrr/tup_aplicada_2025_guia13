6c54e57671f6184500e69badd58779239cdbc365
adlfvrr <adolfover4@gmail.com>
2025-10-30 13:49:36 -0300

    -

---
+++ GUIA_13/GeometriaClientRestAPIDesktop/FormPrincipal.cs
@@ -0,0 +1,10 @@
+namespace GeometriaClientRestAPIDesktop
+{
+    public partial class FormPrincipal : Form
+    {
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GUIA_13/GeometriaRestAPIWeb/DTOs/FiguraDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace GeometriaRestAPIWeb.DTOs
+{
+    public class FiguraDTO
+    {
+        public int? Id { get; set; }
+        public int Tipo { get; set; }
+        public double Largo { get; set; }
+        public double Ancho { get; set; }
+        public double Radio { get; set; }
+        public double Area { get; set; }
+    }
+}
